630fb98f58c1738150b28965cb3bf6101fc65528
Thilo Bronnenmeyer <t.bronnenmeyer@gmail.com>
2025-04-13 20:13:36 +0200

    patch: add simple history to CLI chat example and fix bugs

--- src/pubsubbud/helpers.py
+++ src/pubsubbud/helpers.py
@@ -1,9 +1,16 @@
+import time
 import uuid
+from typing import Any
 
 
-def create_header(channel: str) -> dict[str, str]:
-    header = {}
+def get_current_timestamp() -> float:
+    return time.time()
+
+
+def create_header(channel: str, origin_id: str = "pubsub") -> dict[str, Any]:
+    header: dict[str, Any] = {}
     header["message_id"] = str(uuid.uuid4())
     header["channel"] = channel
-    header["origin_id"] = "pubsub"
+    header["origin_id"] = origin_id
+    header["timestamp"] = get_current_timestamp()
     return header
